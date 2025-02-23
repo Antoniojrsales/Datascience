72ad677c26008cb82a3191e5b8e2ef0059d62960
Antoniojrsales <Antoniogomes.junio@gmail.com>
2025-02-23 19:33:19 -0300

    analise de dados breast_cancer

--- Proj_IrisSetosa/Proj_Iris_setosa/Aula2_machine_learning.py
+++
@@ -1,6 +0,0 @@
-#https://www.invertexto.com/fundamentosaula02
-#https://docs.google.com/presentation/d/1wzwsugIEBU8tP0Gt4gLTb6h6mvnOKuuw/edit#slide=id.g1e1c6646caf_0_142
-
-'''1-Realize a leitura do dataset breast_cancer e faça uma amostragem estratificada de 70% que deve ser usado para ajustar o algoritmo e os 30% restantes devem ser usados para medir a performance.
-2-Usando os dados amostrais, use os algoritmos Gaussian Naive Bayes, SVM, Decision tree e Random Forest para fazer a classificação ( em duas classes: maligno e benigno).
-3-Calcule a performance do algoritmo naive bayes (acertos/total) e indique qual obteve o melhor resultado.'''
